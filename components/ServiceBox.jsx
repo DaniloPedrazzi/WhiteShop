a9a9cfe64174be852102f09686eb74ebb375b422
Danilo <danilo.justino1@gmail.com>
2022-06-09 21:59:20 -0300

    Remove Truck and Code

--- components/ServiceBox.jsx
+++ components/ServiceBox.jsx
@@ -1,5 +1,3 @@
-import truck from "../Images/truck.svg";
-import code from "../Images/code.svg";
 import Image from "next/image";
 
 export function ServiceBox(titulo, preco, descricao) {
@@ -8,14 +6,6 @@ export function ServiceBox(titulo, preco, descricao) {
       <h3>{titulo}</h3>
       <h3 className="preço">R${preco}</h3>
       <p>{descricao}</p>
-      <ul>
-        <li>
-          <Image src={truck} height={15} width={15} /> 15 Dias
-        </li>
-        <li className="code">
-          <Image src={code} height={15} width={15} /> React
-        </li>
-      </ul>
       <a className="Detalhes_Btn" href="#">
         Detalhes
       </a>
--- pages/LogIn.jsx
+++ pages/LogIn.jsx
@@ -1,11 +1,83 @@
 import { Header } from "../components/Header";
+import { useState } from "react";
+import { db } from "../firebase";
+import { collection, addDoc, onSnapshot, orderBy, query } from "firebase/firestore";
 import Link from "next/link";
+import { useEffect } from "react";
 
 function LogIn() {
+  const [users, setUsers] = useState([]);
+  const [id, setId] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "users")),
+        (snapshot) => {
+          setUsers(snapshot.docs);
+        }
+      ),
+    [db]
+  );
+
+  const logar = async () => {
+    if (loading == true) {
+      console.log("Carregando, aguarde...");
+      return;
+    }
+    if (!validateEmail(email)) {
+      console.log("email inválido");
+      return;
+    }
+    setLoading(true);
+
+    users.map((user) => (
+      setId(user.id),
+      setEmail(user.email),
+      setSenha(user.senha)
+    ))
+    console.log(id + email + senha);
+
+    //limpa os inputs
+    setEmail("");
+    setSenha("");
+    setLoading(false);
+  };
+
   return (
     <div>
       <Header />
-
+      <div>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+          required
+        />
+        <button
+          disabled={
+            !email.trim() &&
+            !senha.trim() /*espaço não conta como caractere*/
+          }
+          onClick={logar}
+        >
+          LogIn
+        </button>
+      </div>
       <Link href="/Cadastro">Cadastro</Link>
     </div>
   );
